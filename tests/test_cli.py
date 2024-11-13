cc0e1b808ae902caf09f42f636348a9e7c2480ea
Emily McLean <emilymcleandeveloper@gmail.com>
2024-11-13 19:01:14 +1100

    I'm pretty sure I already tried this

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -8,11 +8,10 @@ from src import from_local
 class TestCli(unittest.TestCase):
 
     def test_local(self):
-        print(Path("fixtures/action.yml").resolve())
         runner = CliRunner()
-        result = runner.invoke(from_local, ["--pkl-github-actions-bindings", "fixtures/action.yml", "actions/checkout@v4"])
+        result = runner.invoke(from_local, ["--pkl-github-actions-bindings", "tests/fixtures/action.yml", "actions/checkout@v4"])
 
-        with Path("fixtures/action.pkl").open() as f:
+        with Path("tests/fixtures/action.pkl").open() as f:
             expected_output = f.read()
 
         self.assertEqual(0, result.exit_code)
