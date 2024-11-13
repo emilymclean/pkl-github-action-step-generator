dc8232ed9bd28acb61994706123c8ae6f10562fb
Emily McLean <emilymcleandeveloper@gmail.com>
2024-11-13 18:58:31 +1100

    Actually fix test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -9,9 +9,9 @@ class TestCli(unittest.TestCase):
 
     def test_local(self):
         runner = CliRunner()
-        result = runner.invoke(from_local, ["--pkl-github-actions-bindings", "tests/fixtures/action.yml", "actions/checkout@v4"])
+        result = runner.invoke(from_local, ["--pkl-github-actions-bindings", "fixtures/action.yml", "actions/checkout@v4"])
 
-        with Path("tests/fixtures/action.pkl").open() as f:
+        with Path("fixtures/action.pkl").open() as f:
             expected_output = f.read()
 
         self.assertEqual(0, result.exit_code)
